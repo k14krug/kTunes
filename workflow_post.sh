276778193de62950bdd3cca895ce35f33adcec7b
k14krug <kenkrug@yahoo.com>
2022-09-28 16:06:58 -0700

    commit

--- workflow_post.sh
+++ workflow_post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Enter playlist name you created and want to copy to the iTunes directory"
-ls -l ./*m3u
+echo "Enter playlist name from list below that you want copied to the iTunes directory"
+ls  ./*m3u>junk;cat junk;rm junk
 read playlist
 if [ -f "$playlist" ]; then
    echo "File $playlist found, will covert unix to dos then copy to iTunes lib"
@@ -12,4 +12,8 @@ fi
 unix2dos $playlist
 echo "copying $1.m3u to /mnt/c/users/nwkru/music/itunes/"
 cp -p  $playlist /mnt/c/users/nwkru/music/itunes/
-
+echo "# # # # # # # # # # # # # # # # # # # #"
+echo "# "
+echo "# Now you must go to your itunes directory and drag $playlist on top of ImportM3U.vbs to import the playlist into itunes"
+echo "# "
+echo "# # # # # # # # # # # # # # # # # # # # "
