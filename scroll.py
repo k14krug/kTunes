6474f01d9b8e920b56654c58d2ec528966b500b0
Your Name <you@example.com>
2023-03-30 16:19:04 -0700

    Updated scrolls

--- scroll.py
+++ scroll.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -7,33 +7,42 @@ app = Flask(__name__)
 def create_playlist():
     pass
 
-@app.route('/tracks')
-def tracks():
+# Helper function to get the total number of tracks in the database
+def get_total_tracks():
     conn = sqlite3.connect('kTunes.sqlite')
     cursor = conn.cursor()
-
-    # Get the total number of tracks
     cursor.execute('SELECT COUNT(*) FROM tracks')
     total_tracks = cursor.fetchone()[0]
+    conn.close()
+    return total_tracks
 
-    # Get the first 20 tracks
-    cursor.execute('SELECT * FROM tracks LIMIT 20')
-    tracks = cursor.fetchall()
-    #print("Tracks=",tracks)
-
-    return render_template('scroll.html', tracks=tracks, total_tracks=total_tracks)
-
-@app.route('/load-more-tracks/<int:offset>')
-def load_more_tracks(offset):
+# Helper function to get a page of tracks from the database
+def get_tracks(start, limit):
     conn = sqlite3.connect('kTunes.sqlite')
     cursor = conn.cursor()
-
-    # Get the next 20 tracks starting from the given offset
-    cursor.execute('SELECT * FROM tracks LIMIT 20 OFFSET ?', (offset,))
+    cursor.execute('SELECT artist, song, category, album, play_cnt, last_play_dt, date_added FROM tracks ORDER BY artist LIMIT ?, ?', (start, limit))
     tracks = cursor.fetchall()
+    conn.close()
+    return tracks
 
-    return jsonify({'html': render_template('tracks-list.html', tracks=tracks)})
-
+# Route for the tracks page
+@app.route('/tracks')
+def tracks():
+    # Get the page number from the request args, default to 1 if not present
+    page = request.args.get('page', 1, type=int)
+    # Set the number of tracks per page
+    per_page = 20
+    # Calculate the starting index of the tracks for the current page
+    start = (page - 1) * per_page
+    # Get the total number of tracks in the database
+    total_tracks = get_total_tracks()
+    # Get the tracks for the current page
+    tracks = get_tracks(start, per_page)
+    # Calculate the total number of pages based on the number of tracks per page
+    total_pages = (total_tracks + per_page - 1) // per_page
+    # Render the template with the tracks and pagination information
+    print("before render_template, tracks=",tracks)
+    return render_template('scroll.html', tracks=tracks, total_pages=total_pages)
 
 if __name__ == '__main__':
     app.run(debug=True)
